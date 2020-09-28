7f85377c238eaab377853c16bc6d55c7e85436fa
Oscar Araya <osaronaragar15@gmail.com>
2020-09-28 16:17:01 -0600

    [NUEVO]: 1. Se agregan las funciones restantes para cumplir totalmente con la especificacion requerida. 2. Se hacen pruebas de todo el codigo. Todo esta totalmente funcional, a excepcion de la funcion de encontrarPedidos. Espero a revisarla con la parte de frontend para decidir como solucionarla. [CAMBIOS]: 1. Se corrigen errores menores de utilizacion de rutas. 2. Se generalizan algunas partes del codigo y se encapsulan dentro de funciones para no reescribir funcionamiento de manera innecesaria. 3. Se mejoran los bucles iterativos para que sean mas eficientes al buscar, eliminar, actualizar y agregar informacion en las bases de datos.

---
+++ BackendAgroTICO/Venta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendAgroTICO
+{
+    class Venta
+    {
+        public int numeroCedulaComprador;
+        public String codigoFactura;
+        public int anioCompra;
+        public int mesCompra;
+        public int diaCompra;
+        public int calificacionGeneral;
+        public String direccionEntrega;
+
+        public int montoTotal;
+
+        public String[] productoVendido;
+
+
+        public Venta()
+        {
+        }
+    }
+}
